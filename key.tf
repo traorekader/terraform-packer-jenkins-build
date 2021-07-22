7ca30cd1514d876a393f27b26bb500d5519a75a6
Kader Youssouf TRAORE <kader.traore@Kaders-MacBook-Pro.local>
2021-07-22 18:06:10 +0200

    fix the key.tf issue

--- key.tf
+++ key.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "mykeypair" {
     key_name = "mykeypair"
-    public_key = file("./mykey.pub")
-    #public_key = fileexists(var.PATH_TO_PUBLIC_KEY) ? file(var.PATH_TO_PUBLIC_KEY) : var.DUMMY_SSH_PUB_KEY
+    # public_key = file("./mykey.pub")
+    public_key = fileexists(var.PATH_TO_PUBLIC_KEY) ? file(var.PATH_TO_PUBLIC_KEY) : var.DUMMY_SSH_PUB_KEY
 }
