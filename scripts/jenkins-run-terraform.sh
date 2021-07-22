41db9d078851e18ad31b4fc03d8f997834d55269
Kader Youssouf TRAORE <kader.traore@Kaders-MacBook-Pro.local>
2021-07-22 16:50:21 +0200

    modified scripts/jenkins-run-terraform.sh

--- scripts/jenkins-run-terraform.sh
+++ scripts/jenkins-run-terraform.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 AWS_REGION="eu-west-1"
-cd jenkins-packer-demo
+# cd jenkins-packer-demo
+echo $(pwd)
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
